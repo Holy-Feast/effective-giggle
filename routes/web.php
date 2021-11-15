c587fc237535a342f096a6dadf96cd8ee0301b65
Holy-Feast <shelofasstov@gmail.com>
2021-11-15 11:20:55 +0200

    hello world check

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/look/', function () {
+    return '<h1>Hello world?</h1>';
+});
 
 Auth::routes();
 
